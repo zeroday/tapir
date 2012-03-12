7f2bfeaf534b6d57adae74c17991f2dbc9ebacc6
Jonathan Cran <jcran@0x0e.org>
2012-03-12 02:32:11 -0500

    don't forget to require

---
+++ lib/client/test.rb
@@ -0,0 +1,14 @@
+$:.unshift(File.dirname(__FILE__))
+
+require 'test/test_bing_search_service'
+require 'test/test_corpwatch_service'
+require 'test/test_foursquare_webclient'
+require 'test/test_google_search_scraper'
+require 'test/test_google_search_service'
+require 'test/test_google_webclient'
+require 'test/test_myspace_webclient'
+require 'test/test_soundcloud_webclient'
+require 'test/test_twitpic_scraper'
+require 'test/test_twitpic_webclient'
+require 'test/test_twitter_webclient'
+
